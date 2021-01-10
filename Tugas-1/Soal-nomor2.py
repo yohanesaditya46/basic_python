12121fbe3e9e431bc731537341f12ecd2fe39d9e
yohanesaditya <wisnuwardana2509@gmail.com>
2021-01-10 15:47:35 +0700

    Assignment-1

--- Tugas-1/Soal-nomor2.py
+++ Tugas-1/Soal-nomor2.py
@@ -1,5 +1,5 @@
 pi = 22/7
-r = float (input())
-luas = pi * r * r
+r = float (input("Masukkan jari -jari lingkaran\t: "))
+luas = pi * r**2
 
 print ("Luas lingkaran dengan jari - jari " + str(r) + "cm" + " " + "adalah" + " " + str(round(luas,2)) + "cm\u00b2" )
